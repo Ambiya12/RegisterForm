0938658abad5ed3b2f854913c06a207ec2229537
Ambiya12 <ambiyadms@gmail.com>
2025-03-25 14:28:49 +0100

    Implement registration form with state management and API integration

--- src/pages/RegisterForm.jsx
+++ src/pages/RegisterForm.jsx
@@ -1,6 +1,30 @@
-import React from 'react';
+import {useState} from 'react';
+import axios from 'axios'
+
 
 const RegisterForm = () => {
+    const [first_name, setFirst_Name] = useState('')
+    const [last_name, setLast_Name] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    console.log(first_name, last_name, email, password)
+
+    const handleRegistration = async (e) => {
+        e.preventDefault()
+        try {
+            const newUser = await axios.post('http://localhost:8000/api/register/', {first_name, last_name, email, password})
+            if(newUser.status === 201){
+                alert(newUser.data)
+            }
+        }
+        catch(err){
+            console.log(err.response.data)
+            if(err){
+                alert(err.response.data)
+            }
+        }
+    }
     return (
         <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -10,10 +34,10 @@ const RegisterForm = () => {
             </div>
 
             <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                <form className="space-y-6" action="#" method="POST">
+                <form onSubmit={handleRegistration} className="space-y-6" action="#" method="POST">
                     <div>
                         <label
-                            htmlFor="first-name"
+                            htmlFor="first_name"
                             className="block text-sm/6 font-medium text-gray-900"
                         >
                             First name
@@ -21,18 +45,19 @@ const RegisterForm = () => {
                         <div className="mt-2">
                             <input
                                 type="text"
-                                name="first-name"
-                                id="first-name"
+                                name="first_name"
+                                id="first_name"
                                 autoComplete="given-name"
                                 required
                                 className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                onChange={e => setFirst_Name(e.target.value)}
                             />
                         </div>
                     </div>
 
                     <div>
                         <label
-                            htmlFor="last-name"
+                            htmlFor="last_name"
                             className="block text-sm/6 font-medium text-gray-900"
                         >
                             Last name
@@ -40,12 +65,13 @@ const RegisterForm = () => {
                         <div className="mt-2">
                             <input
                                 type="text"
-                                name="last-name"
-                                id="last-name"
+                                name="last_name"
+                                id="last_name"
                                 autoComplete="family-name"
                                 required
                                 className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                            />
+                                onChange={e => setLast_Name(e.target.value)}
+                                />
                         </div>
                     </div>
 
@@ -64,7 +90,8 @@ const RegisterForm = () => {
                                 autoComplete="email"
                                 required
                                 className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                            />
+                                onChange={e => setEmail(e.target.value)}
+                                />
                         </div>
                     </div>
 
@@ -85,7 +112,8 @@ const RegisterForm = () => {
                                 autoComplete="current-password"
                                 required
                                 className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                            />
+                                onChange={e => setPassword(e.target.value)}
+                                />
                         </div>
                     </div>
 
@@ -98,6 +126,10 @@ const RegisterForm = () => {
                         </button>
                     </div>
                 </form>
+                <p className="mt-10 text-center text-sm/6 text-gray-500">
+                    Already a member?
+                        <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500"> Sign in here</a>
+                </p>
             </div>
         </div>
     );
