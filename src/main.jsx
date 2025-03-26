3f5a4e3ec0ba635da9333afa73e06c5d03f92562
Ambiya12 <ambiyadms@gmail.com>
2025-03-26 09:48:33 +0100

    Wrap application in ServicesController and AuthController for context management

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import MyRouter from './MyRouter'
-import { BrowserRouter } from 'react-router'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import MyRouter from "./MyRouter";
+import { BrowserRouter } from "react-router";
+import { ServicesController } from "../context/servicesContext.jsx";
+import { AuthController } from "../context/authContext.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <MyRouter />
-  </BrowserRouter>
-)
+createRoot(document.getElementById("root")).render(
+  <ServicesController>
+    <AuthController>
+      <BrowserRouter>
+        <MyRouter />
+      </BrowserRouter>
+    </AuthController>
+  </ServicesController>
+);
