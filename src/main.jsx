df0bb6d805e64a3129e17b60567e1139df53bf54
Ambiya12 <ambiyadms@gmail.com>
2025-03-26 13:39:07 +0100

    Fix import paths and reorder context providers in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import MyRouter from "./MyRouter";
 import { BrowserRouter } from "react-router";
-import { ServicesController } from "../context/servicesContext.jsx";
-import { AuthController } from "../context/authContext.jsx";
+import { ServicesController } from "./context/servicesContext.jsx";
+import { AuthController } from "./context/authContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <ServicesController>
-    <AuthController>
-      <BrowserRouter>
+  <BrowserRouter>
+    <ServicesController>
+      <AuthController>
         <MyRouter />
-      </BrowserRouter>
-    </AuthController>
-  </ServicesController>
+      </AuthController>
+    </ServicesController>
+  </BrowserRouter>
 );
