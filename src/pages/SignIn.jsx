35ab19aed377d584828a481119a00adaaf74112e
Ambiya12 <ambiyadms@gmail.com>
2025-03-26 13:39:48 +0100

    Refactor SignIn component to use AuthContext for handling sign-in and improve form structure

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,51 +1,37 @@
-import { useState } from 'react';
-import axios from 'axios';
+import { useContext, useState } from 'react';
+import { AuthContext } from '../context/authContext.jsx';
 
 const SignIn = () => {
+    const {handleSignIn} = useContext(AuthContext)
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    console.log(email, password);
-
-    const handleSignIn = async (e) => {
-        e.preventDefault();
-        try {
-            const user = await axios.post('http://localhost:8000/api/login/', { email, password });
-            if (user.status === 200) {
-                alert('Login successful');
-            }
-        } catch (err) {
-            console.log(err.response.data);
-            if (err) {
-                alert(err.response.data);
-            }
-        }
-    };
-
     return (
-        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <img
+                    alt="Your Company"
+                    src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+                    className="mx-auto h-10 w-auto"
+                />
                 <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-                    Sign In
+                    Sign in to your account
                 </h2>
             </div>
 
             <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                <form onSubmit={handleSignIn} className="space-y-6" action="#" method="POST">
+                <form onSubmit={(e) => handleSignIn(e, email, password)} className="space-y-6" action="#" method="POST">
                     <div>
-                        <label
-                            htmlFor="email"
-                            className="block text-sm/6 font-medium text-gray-900"
-                        >
+                        <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
                             Email address
                         </label>
                         <div className="mt-2">
                             <input
-                                type="email"
-                                name="email"
                                 id="email"
-                                autoComplete="email"
+                                name="email"
+                                type="email"
                                 required
+                                autoComplete="email"
                                 className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                                 onChange={(e) => setEmail(e.target.value)}
                             />
@@ -54,20 +40,22 @@ const SignIn = () => {
 
                     <div>
                         <div className="flex items-center justify-between">
-                            <label
-                                htmlFor="password"
-                                className="block text-sm/6 font-medium text-gray-900"
-                            >
+                            <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
                                 Password
                             </label>
+                            <div className="text-sm">
+                                <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                                    Forgot password?
+                                </a>
+                            </div>
                         </div>
                         <div className="mt-2">
                             <input
-                                type="password"
-                                name="password"
                                 id="password"
-                                autoComplete="current-password"
+                                name="password"
+                                type="password"
                                 required
+                                autoComplete="current-password"
                                 className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                                 onChange={(e) => setPassword(e.target.value)}
                             />
@@ -79,19 +67,19 @@ const SignIn = () => {
                             type="submit"
                             className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                         >
-                            Sign In
+                            Sign in
                         </button>
                     </div>
                 </form>
+
                 <p className="mt-10 text-center text-sm/6 text-gray-500">
                     Not a member?{' '}
-                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                        Sign up here
+                    <a href="/register" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                        Start a 14 day free trial
                     </a>
                 </p>
             </div>
         </div>
-    );
-};
-
+    )
+}     
 export default SignIn;
